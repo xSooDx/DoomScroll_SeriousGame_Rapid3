985c461cf8a5afd877431ddc2415ab7a46d3e91d
d-dcarl <47509902+d-dcarl@users.noreply.github.com>
2022-11-06 22:34:46 -0700

    working on scenarios

---
+++ Assets/Scripts/ScenarioScripts/OpenCloseDoor.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenCloseDoor : MonoBehaviour
+{
+    public Vector3 closedDoor;
+    public Vector3 openedDoor;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
