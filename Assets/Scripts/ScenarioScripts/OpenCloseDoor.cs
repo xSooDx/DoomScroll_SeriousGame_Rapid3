b98c60603fd12cb3fa75c4d87c40abaef3e0ceb7
d-dcarl <47509902+d-dcarl@users.noreply.github.com>
2022-11-08 09:01:34 -0700

    progress

--- Assets/Scripts/ScenarioScripts/OpenCloseDoor.cs
+++ Assets/Scripts/ScenarioScripts/OpenCloseDoor.cs
@@ -1,15 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class OpenCloseDoor : MonoBehaviour
 {
-    public Vector3 closedDoor;
-    public Vector3 openedDoor;
+    public GameObject doortoRotate;
+    public float openSpeed;
+    private bool isOpen = false;
+    public AudioSource openSound;
+    public AudioSource closeSound;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyUp(KeyCode.O))
+            {
+                if (isOpen == false)
+                {
+                    transform.Rotate(0, 90, 0);
+                    isOpen = true;
+                    //openSound.Play();
+                } else
+                {
+                    transform.Rotate(0, -90, 0);
+                    isOpen = false;
+                    closeSound.Play();
+                }
+                
+            }
+
     }
 }
